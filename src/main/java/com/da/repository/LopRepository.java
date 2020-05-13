0ca23eda51f1ade1d7429e422d0797a93bb5085c
dangtuan096 <tuanmanh322@gmail.com>
2020-05-13 11:40:22 +0700

    fix something

--- src/main/java/com/da/repository/LopRepository.java
+++ src/main/java/com/da/repository/LopRepository.java
@@ -3,9 +3,14 @@ package com.da.repository;
 import com.da.model.Lop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LopRepository extends JpaRepository<Lop, Integer>, JpaSpecificationExecutor<Lop> {
-
+    @Query(
+            nativeQuery = true,
+            value = "select lo from lop as lo where lo.tenlop =?1"
+    )
+    Lop findByTenlop(String tenLop);
 }
--- src/main/java/com/da/repository/MonhocRepository.java
+++ src/main/java/com/da/repository/MonhocRepository.java
@@ -3,9 +3,12 @@ package com.da.repository;
 import com.da.model.Monhoc;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MonhocRepository extends JpaRepository<Monhoc, Integer>, JpaSpecificationExecutor<Monhoc> {
+    @Query(nativeQuery = true,value = " select mh from monhoc as mh where mh.ma_monhoc=?1")
+    Monhoc findByTenmonhoc(String tenMonHoc);
 
 }
